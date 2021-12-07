ef1200aa532e4296d565693e9f1e7daf60c703be
ParrucciD <91885547+ParrucciD@users.noreply.github.com>
2021-12-07 15:23:04 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
+float magg(float array[], int dimensione){
+    float max=array[0];
+    for( int i=0;i<dimensione;i++){
+
+        if(array[i]>max){
+            max=array[i];
+        }
+    }
+return max;
+}
+
 
