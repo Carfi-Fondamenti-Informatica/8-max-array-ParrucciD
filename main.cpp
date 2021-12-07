575ae4f759e60bc6d94220c83263815b5948adaa
ParrucciD <91885547+ParrucciD@users.noreply.github.com>
2021-12-07 15:24:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    float max;
+    float array[n];
+    for(int i=0;i<n;i++){
+        cin >> array[i];
+    }
+    max= magg(array,n);
+    cout << max << endl;
+    return 0;
 }
